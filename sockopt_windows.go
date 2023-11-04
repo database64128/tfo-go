ce91df710ad6335302e071d74df50bb6df2913f4
database64128 <free122448@hotmail.com>
2023-11-05 03:18:22 +0800

    🔢 windows: use upstreamed sockopt number

--- sockopt_windows.go
+++ sockopt_windows.go
@@ -2,8 +2,6 @@ package tfo
 
 import "golang.org/x/sys/windows"
 
-const TCP_FASTOPEN = 15
-
 func setTFO(fd, value int) error {
-	return windows.SetsockoptInt(windows.Handle(fd), windows.IPPROTO_TCP, TCP_FASTOPEN, value)
+	return windows.SetsockoptInt(windows.Handle(fd), windows.IPPROTO_TCP, windows.TCP_FASTOPEN, value)
 }
