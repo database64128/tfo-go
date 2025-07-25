f70ecf8ae7dfb2e0f691d3c5aa3bd66d245b2721
database64128 <free122448@hotmail.com>
2025-07-25 15:38:02 +0800

    6️⃣ windows: sync structs with gotip 1.26
    - https://github.com/golang/go/commit/f7432e0230eef9f40117a901bc71e09db6432033
    - https://github.com/golang/go/commit/f11599b0b909a61d575bcea532ee2250f9a682da

--- netpoll_windows_checklinkname0.go
+++ netpoll_windows_checklinkname0.go
@@ -10,41 +10,6 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-// operation contains superset of data necessary to perform all async IO.
-//
-// Copied from src/internal/poll/fd_windows.go
-type operation struct {
-	// Used by IOCP interface, it must be first field
-	// of the struct, as our code relies on it.
-	o windows.Overlapped
-
-	// fields used by runtime.netpoll
-	runtimeCtx uintptr
-	mode       int32
-
-	// fields used only by net package
-	fd     *pFD
-	buf    windows.WSABuf
-	msg    windows.WSAMsg
-	sa     windows.Sockaddr
-	rsa    *windows.RawSockaddrAny
-	rsan   int32
-	handle windows.Handle
-	flags  uint32
-	qty    uint32
-	bufs   []windows.WSABuf
-}
-
-//go:linkname execIO internal/poll.execIO
-func execIO(o *operation, submit func(o *operation) error) (int, error)
-
-func (fd *pFD) ConnectEx(ra windows.Sockaddr, b []byte) (n int, err error) {
-	n, err = execIO(&fd.wop, func(o *operation) error {
-		return windows.ConnectEx(o.fd.Sysfd, ra, &b[0], uint32(len(b)), &o.qty, &o.o)
-	})
-	return
-}
-
 // Network file descriptor.
 //
 // Copied from src/net/fd_posix.go
